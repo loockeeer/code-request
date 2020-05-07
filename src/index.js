389aed8d7b20b3f4066fe8a865febf12bfcc60e6
loockeeer <lulu.r1313@gmail.com>
2020-05-07 20:53:58 +0200

    Add REST support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
+const http = require('http');
+
 const Bind = require('./Bind.js')
 
 const config = require('../config.json')
@@ -28,15 +30,43 @@ const resolvers = {
             const {lang, code} = args
 
             const bind = binds.get(lang)
-            if(!bind) return new Error('Language is not specified in config.')
+            if(!bind) return new Error('Bad lang')
 
             return await bind.run(code)
         }
     }
 }
 
+const restServer = http.createServer(async (req, res) => {
+    const args = new URL(req.url, `http://${req.headers.host}`).searchParams;
+    const code = args.get('code')
+    const lang = args.get('lang')
+    if(!code) {
+        res.writeHead(400, { 'Content-Type': 'text/json' })
+        return res.end(JSON.stringify({code: 400, message: "Missing code"}))
+    }
+    if(!lang) {
+        res.writeHead(400, { 'Content-Type': 'text/json' })
+        return res.end(JSON.stringify({code: 400, message: "Missing lang"}))
+    }
+    console.log(lang)
+    const bind = binds.get(lang)
+    if(!bind)  {
+        res.writeHead(400, { 'Content-Type': 'text/json' })
+        return res.end(JSON.stringify({code: 400, message: "Bad lang"}))
+    }
+
+    const result = await bind.run(code)
+    res.writeHead(200, {'Content-Type': 'text/json'})
+    return res.end(JSON.stringify({code: 200, data: result}))
+});
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen({port: config.port, host: config.host}).then(({ url }) => {
-  console.log(`=> Server ready at ${url}`);
+restServer.listen(config.rest.port, config.rest.host, ()=>{
+    console.log(`=> REST Server ready at http://${config.rest.host}:${config.rest.port}`)
+});
+
+server.listen({port: config.graphql.port, host: config.graphql.host}).then(({ url }) => {
+  console.log(`=> GraphQL Server ready at ${url}`);
 });
