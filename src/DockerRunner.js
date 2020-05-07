575da5209c7223a6e59fdcfb9bbc49308b807bd7
loockeeer <lulu.r1313@gmail.com>
2020-05-07 23:31:15 +0200

    CPU / Memory usage limits + network on/off

--- src/DockerRunner.js
+++ src/DockerRunner.js
@@ -6,6 +6,11 @@ module.exports = class DockerRunner extends EventEmitter{
         this.args = ["run"]
         this.args.push("-w","/usr/src/app")
         this.args.push("--name", id)
+        
+        if(config.container.permissions.network) this.args.push('--network', config.container.permissions.network ? 'bridge' : 'none')
+        if(config.container.limits.cpu) this.args.push('--cpu-quota', config.container.limits.cpu*1000)
+        if(config.container.limits.memory) this.args.push('-m', config.container.limits.memory)
+
         this.args.push("-v", `${__dirname}/run/${id}/:/usr/src/app`)
         this.args.push(image)
         this.args.push("sh", "-c", commands.join("&&"))
