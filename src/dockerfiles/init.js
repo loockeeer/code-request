cfb767e6351c2a7c69a54f0e09940dff7d058df7
loockeeer <lulu.r1313@gmail.com>
2020-05-07 20:53:27 +0200

    add __dirname

--- src/dockerfiles/init.js
+++ src/dockerfiles/init.js
@@ -2,12 +2,17 @@ const {exec} = require("child_process")
 const fs = require('fs').promises
 
 async function main() {
-    const files = (await fs.readdir('.')).filter(file=>!file.endsWith('.js'))
+    const files = (await fs.readdir(__dirname)).filter(file=>!file.endsWith('.js'))
 
     files.forEach(file=>{
         console.log('[Dockerfiles INIT] Building '+file+'...')
-        exec("docker build - < ./"+file+" -t docker_api_code_runner-"+file, null, (err, stdout, stderr)=>{
-            console.log('[Dockerfiles INIT] '+file+' built.')
+        exec("docker build - < "+__dirname+"/"+file+" -t coderequest-"+file, null, (err, stdout, stderr)=>{
+
+            if(stderr) {
+                console.error('[Dockerfiles INIT] Unable to build '+file)
+                console.error(stderr)
+            } else console.log('[Dockerfiles INIT] '+file+' built.')
         })
     })
 }
+main()
